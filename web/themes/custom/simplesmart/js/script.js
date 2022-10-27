a84b0ff91f31522cefbc7603571a69ba93f7047e
Junior Dering <juniordering@hotmail.com>
2022-10-26 21:53:36 -0400

    improving css and js

--- web/themes/custom/simplesmart/js/script.js
+++ web/themes/custom/simplesmart/js/script.js
@@ -1,1 +1,10 @@
 console.log('oiiimundo')
+let button_home = document.getElementsByClassName('button-home')[0]
+let button_layout = document.getElementsByClassName('button-layout')
+console.log(button_home.innerHTML);
+var url_atual = window.location.href;
+console.log(url_atual)
+// button_layout.addEventListener('click', function(){
+//   console.log(button_layout);
+// })
+
